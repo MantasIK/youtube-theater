fdead9eb53d07d5c7b80bcb8c48ed814d589b7ec
Mantas (Matt) <54780953+MantasIK@users.noreply.github.com>
2021-03-10 21:33:20 -0500

    Update Display.js

--- src/components/Display.js
+++ src/components/Display.js
@@ -6,6 +6,7 @@ import VideoList from "./VideoList";
 const Display = (props) => {
   const [currentVideo, setVideo] = useState("9E6b3swbnWg");
   const changeVideo = (video) => {
+    window.scrollTo({ top: 0, left: 0 });
     setVideo(video);
   };
 
